34f3256219a4881e9165f38d7daea6764e10fe5d
shint1015 <53039633+shint1015@users.noreply.github.com>
2023-03-21 00:57:30 +0900

    [update]props

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 const App = () => {
+    const profiles = [
+        {name: "Taro", age: 10},
+        {name: "Hanako", age: 5},
+        {name: "Sum"},
+    ]
   return (
       <div>
-          <Cat/>
-          <Cat/><Cat/><Cat/>
+          {
+              profiles.map((profile, index) => {
+                  return <User name={profile.name} age={profile.age} key={index} />
+              })
+          }
       </div>
   )
 }
 
-const Cat = () => {
-    return <div>Meow!</div>
+const User = (props) => {
+    return <div>Hi, I am {props.name}, and {props.age} years old!</div>
+}
+User.defaultProps = {
+    age: 1
 }
 export default App
